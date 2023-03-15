e3bea853ab48dbecd302b22b4f32abc13dc8f8c9
anisia1204 <anisia.rosu@student.upt.ro>
2023-03-15 18:30:42 +0200

    created Book class

---
+++ src/main/java/com/pergamumlibraryness/Book.java
@@ -0,0 +1,74 @@
+package com.pergamumlibraryness;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+class Book {
+
+    private @Id @GeneratedValue Long id;
+    private String title;
+    private String author;
+
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o)
+            return true;
+        if (!(o instanceof Book))
+            return false;
+        Book book= (Book) o;
+        return Objects.equals(this.id, book.id) && Objects.equals(this.title, book.title)
+                && Objects.equals(this.author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.title, this.author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
