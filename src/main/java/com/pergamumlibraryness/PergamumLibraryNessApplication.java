3831b0fc6fbdf6a0bda5fb8641213defb436c635
anisia1204 <anisia.rosu@student.upt.ro>
2023-03-15 18:24:42 +0200

    Initial commit

---
+++ src/main/java/com/pergamumlibraryness/PergamumLibraryNessApplication.java
@@ -0,0 +1,13 @@
+package com.pergamumlibraryness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PergamumLibraryNessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PergamumLibraryNessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pergamumlibraryness/PergamumLibraryNessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pergamumlibraryness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PergamumLibraryNessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
